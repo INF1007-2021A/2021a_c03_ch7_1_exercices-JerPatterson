b88e96052ebab83269f147fa59bce90ae21edf51
JerPatterson <you@example.com>
2021-10-31 20:24:27 -0400

    all exercises done

--- exercice.py
+++ exercice.py
@@ -4,20 +4,72 @@
 from collections import deque
 
 
-def get_fibonacci_number(TODO):
-	pass
+def get_fibonacci_number(number: int) -> int:
+	if number == 0:
+		return 0
+	elif number == 1:
+		return 1
+	else:
+		return get_fibonacci_number(number - 1) + get_fibonacci_number(number - 2)
+	
 
-def get_fibonacci_sequence(TODO):
-	pass
+def get_fibonacci_sequence(elements: int, sequence: list =[0, 1]) -> list:
+	if elements <= 2:
+		return sequence[0 : elements]
+	if elements == len(sequence):
+		return 
+	else:
+		sequence.append(sequence[len(sequence)-1] + sequence[len(sequence)-2])
+		get_fibonacci_sequence(elements, sequence)
 
-def get_sorted_dict_by_decimals(TODO):
-	pass
+	return sequence
 
-def fibonacci_numbers(length):
-	pass
 
-def build_recursive_sequence_generator(TODO):
-	pass
+def get_sorted_dict_by_decimals(values : dict) -> dict:
+	return dict(sorted(values.items(), key=lambda number: number[1]-int(number[1])))
+
+
+def fibonacci_numbers(length : int) -> int:
+	yield 0
+	yield 1
+	number = 2
+
+	previous_last = 0
+	last = 1
+	while number != length:
+		yield previous_last + last
+		value = previous_last + last
+		previous_last = last
+		last = value
+		number += 1
+
+
+def build_recursive_sequence_generator(first_values : list, fibo_def, sequence_memory : bool =False):
+	def recursive_generator(length):
+		for value in first_values:
+			yield value
+		number = len(first_values)
+		last_values = deque()
+		last_values.extend(first_values)
+
+		if sequence_memory == True:
+			sequence = first_values
+			while number != length:
+				yield fibo_def(last_values)
+				sequence.append(fibo_def(last_values))
+				last_values.append(fibo_def(last_values))
+				last_values.popleft
+				number += 1
+
+		elif sequence_memory == False:
+			while number != length:
+				yield fibo_def(last_values)
+				last_values.append(fibo_def(last_values))
+				last_values.popleft
+				number += 1
+
+	return recursive_generator
+
 
 
 if __name__ == "__main__":
@@ -28,6 +80,7 @@ if __name__ == "__main__":
 	print(get_fibonacci_sequence(1))
 	print(get_fibonacci_sequence(2))
 	print(get_fibonacci_sequence(10))
+	print(get_fibonacci_sequence(20))
 	print()
 
 	spam = {
@@ -57,9 +110,10 @@ if __name__ == "__main__":
 		print(fi, end=" ")
 	print("\n")
 
-	lucas = build_recursive_sequence_generator(TODO)
+	lucas = build_recursive_sequence_generator([2, 1], lambda last_elems: last_elems[-1] + last_elems[-2])
 	print(f"Lucas : {[elem for elem in lucas(10)]}")
-	perrin = build_recursive_sequence_generator(TODO)
+	perrin = build_recursive_sequence_generator([3, 0, 2], lambda last_elems: last_elems[-2] + last_elems[-3])
 	print(f"Perrin : {[elem for elem in perrin(10)]}")
-	hofstadter_q = build_recursive_sequence_generator(TODO)
+	hofstadter_q = build_recursive_sequence_generator([1, 1], lambda last_elems: last_elems[len(last_elems) - last_elems[len(last_elems) - 1]] 
+	+ last_elems[len(last_elems) - last_elems[len(last_elems) - 2]], True)
 	print(f"Hofstadter-Q : {[elem for elem in hofstadter_q(10)]}")
